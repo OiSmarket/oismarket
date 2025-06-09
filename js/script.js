d37708c9fdcb6a2b52caf1c9795a9986b0e24f68
OiSmarket <137676497+OiSmarket@users.noreply.github.com>
2025-06-09 11:28:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,21 +36,6 @@ document.querySelectorAll('.faq-question').forEach(btn => {
     item.classList.toggle('open');
   });
 });
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('signup-form');
-
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Предотвращаем отправку формы по умолчанию
-
-    // Здесь ваш код для выполнения после нажатия на кнопку "Зарегистрироваться"
-    alert('Вы успешно зарегистрировались!'); // Пример: показываем сообщение
-
-    // Дополнительные действия:
-    // - Отправка данных на сервер (AJAX)
-    // - Перенаправление на другую страницу
-    // - Отображение сообщения об успехе
-  });
-});
 
 // Твои данные SendPulse
   const SENDPULSE_CLIENT_ID = '6792a3504cb18f5f17059fd42de4e55a';
